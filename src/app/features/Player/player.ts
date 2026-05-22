83909f55c20c794181683744cc0f90cfdf7adac9
leon-08024 <cauecardosodefranca@gmail.com>
2026-05-22 10:46:52 -0300

    feat: remove cart and payment features; add player component with initial setup

---
+++ src/app/features/Player/player.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-player',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './player.html',
+  styleUrl: './player.scss',
+})
+export class PlayerPageComponent{
+
+}
