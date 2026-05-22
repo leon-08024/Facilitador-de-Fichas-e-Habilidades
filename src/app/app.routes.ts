d6e0aaa2084e7d445d507ba69ec1860e141c1885
leon-08024 <cauecardosodefranca@gmail.com>
2026-05-22 10:57:37 -0300

    feat: update routing by removing cart, payment, and thanks pages; add player page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/Home/home';
 import { MasterPageComponent } from './features/Master/master';
-import { CartPageComponent } from './features/cart/cart';
-import { PaymentPageComponent } from './features/payment/payment';
-import { ThanksPageComponent } from './features/thanks/thanks';
+import { PlayerPageComponent } from './features/Player/player';
+
 
 
 export const routes: Routes = [
@@ -11,7 +10,5 @@ export const routes: Routes = [
     { path: 'home', component: Home },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'master', component: MasterPageComponent },
-    { path: 'cart', component: CartPageComponent },
-    { path: 'pagamento', component: PaymentPageComponent },
-    { path: 'thanks', component: ThanksPageComponent}
+    { path: 'player', component: PlayerPageComponent },
 ];
